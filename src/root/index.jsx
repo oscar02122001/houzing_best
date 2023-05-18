5fe7fe825fbbf41c30e61532f5772d8b7316e71d
oscar02122001 <olimjonismatov01@gmail.com>
2023-05-18 18:18:32 +0500

    filter dropdown

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../index.css";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 import navbar from "../utils/navbar";
 import Navbar from "../components/navbar";
