7f01609ff4f634deedfc5cd62400374019a48d2a
oscar02122001 <olimjonismatov01@gmail.com>
2023-05-28 12:59:55 +0500

    recent section

---
+++ src/components/recent/index.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { Container, Wrapper, Content } from "./style";
+import Card from "../card";
+import Slider from "react-slick";
+
+const { REACT_APP_BASE_URL: url } = process.env;
+
+const settings = {
+  className: "center",
+  centerMode: true,
+  infinite: true,
+  centerPadding: "40px 20px  ",
+  slidesToShow: 3,
+  speed: 500,
+  dots: true,
+};
+const Recent = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`${url}/houses/list`)
+      .then((res) => res.json())
+      .then((res) => setData(res.data || []));
+  }, []);
+
+  return (
+    <Container>
+      <Wrapper>
+        <Content>
+          <h1 className="mainTitle">Recent Properties for Rent</h1>
+          <p className="descript">
+            Nulla quis curabitur velit volutpat auctor bibendum consectetur sit.
+          </p>
+        </Content>
+        <Slider {...settings}>
+          {data.map((value) => {
+            return <Card key={"4"} data={value} margin={"20px"} count={1} />;
+          })}
+        </Slider>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Recent;
